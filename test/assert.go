72b7a88a0574be38ec5e573e651b84f07c526ee9
Jack Barrett <jjbdev21@gmail.com>
2024-12-04 14:59:02 +0000

    Swap actual and expected

--- test/assert.go
+++ test/assert.go
@@ -11,7 +11,7 @@ type Case[A, B any] struct {
 	Expected B
 }
 
-func AssertEqual[A any](t *testing.T, expected A, actual A) {
+func AssertEqual[A any](t *testing.T, actual A, expected A) {
 	if !reflect.DeepEqual(expected, actual) {
 		t.Errorf("Expected %v, got %v", expected, actual)
 	}
@@ -20,6 +20,6 @@ func AssertEqual[A any](t *testing.T, expected A, actual A) {
 func AssertCases[A, B any](t *testing.T, fn func(A) B, cases []Case[A, B]) {
 	for _, c := range cases {
 		fmt.Println(c.Input, c.Expected, fn(c.Input))
-		AssertEqual(t, c.Expected, fn(c.Input))
+		AssertEqual(t, fn(c.Input), c.Expected)
 	}
 }
