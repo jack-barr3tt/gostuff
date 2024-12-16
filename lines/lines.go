9cc88eb509cb364497749cf95ff866091a386a29
Jack Barrett <jjbdev21@gmail.com>
2024-12-16 00:05:46 +0000

    Increase precision of lines package

--- lines/lines.go
+++ lines/lines.go
@@ -1,45 +1,79 @@
 package lines
 
-import "github.com/jack-barr3tt/gostuff/types"
+import (
+	"github.com/jack-barr3tt/gostuff/nums"
+	"github.com/jack-barr3tt/gostuff/types"
+)
 
 type Line struct {
-	m float64
-	c float64
+	mn int
+	md int
+	cn int
+	cd int
+}
+
+func NewFracMC(mn, md, cn, cd int) Line {
+	mgcd := nums.Gcd(mn, md)
+	cgcd := nums.Gcd(cn, cd)
+	if md/mgcd < 0 {
+		mn, md = -mn, -md
+	}
+	if cd/cgcd < 0 {
+		cn, cd = -cn, -cd
+	}
+	return Line{mn / mgcd, md / mgcd, cn / cgcd, cd / cgcd}
 }
 
 func NewYMXC[T ~int | ~float64 | ~int64 | ~float32](m, c T) Line {
-	return Line{float64(m), float64(c)}
+	mn, md := nums.Rationalize(m, 100000)
+	cn, cd := nums.Rationalize(c, 100000)
+	return NewFracMC(mn, md, cn, cd)
 }
 
 func NewBetween(a, b types.Point) Line {
-	m := (float64(b[1]) - float64(a[1])) / (float64(b[0]) - float64(a[0]))
-	c := float64(a[1]) - m*float64(a[0])
-	return Line{m, c}
+	mn := a[1] - b[1]
+	md := a[0] - b[0]
+	cn := a[1]*(a[0]-b[0]) - a[0]*(a[1]-b[1])
+	cd := a[0] - b[0]
+	return NewFracMC(mn, md, cn, cd)
 }
 
 func NewPointDir(p types.Point, d types.Direction) Line {
-	m := float64(d[1]) / float64(d[0])
-	c := float64(p[1]) - m*float64(p[0])
-	return Line{m, c}
+	mn := d[1]
+	md := d[0]
+	cn := p[1]*d[0] - p[0]*d[1]
+	cd := d[0]
+	return NewFracMC(mn, md, cn, cd)
 }
 
 func NewAXBYC[T ~int | ~float64 | ~int64 | ~float32](a, b, c T) Line {
-	return Line{float64(-a) / float64(b), float64(c) / float64(b)}
+	a0, a1 := nums.Rationalize(a, 100000)
+	b0, b1 := nums.Rationalize(b, 100000)
+	c0, c1 := nums.Rationalize(c, 100000)
+
+	mn := -b1 * a0
+	md := a1 * b0
+	cn := c0 * b1
+	cd := c1 * b0
+	return NewFracMC(mn, md, cn, cd)
 }
 
 func (l Line) SubX(x float64) float64 {
-	return l.m*x + l.c
+	return (float64(l.mn)*x)/float64(l.md) + (float64(l.cn) / float64(l.cd))
 }
 
 func (l Line) SubY(y float64) float64 {
-	return (y - l.c) / l.m
+	return ((y - float64(l.cn)/float64(l.cd)) * float64(l.md)) / float64(l.mn)
 }
 
 func (l Line) IntersectsAt(l2 Line) (float64, float64, bool) {
-	if l.m == l2.m {
+	if l.mn == l2.mn && l.md == l2.md {
 		return 0, 0, false
 	}
-	x := (l2.c - l.c) / (l.m - l2.m)
-	y := l.SubX(x)
-	return x, y, true
+	xn := l.md * l2.md * (l.cn*l2.cd - l.cd*l2.cn)
+	xd := l.cd * l2.cd * (l.md*l2.mn - l2.md*l.mn)
+	yn := l.md * l2.md * (l.cd*l2.cn*l.mn*l2.md - l.cn*l2.cd*l.md*l2.mn)
+	yd := l.cd * l2.md * l2.cd * l.md * (l.mn*l2.md - l.md*l2.mn)
+
+	return float64(xn) / float64(xd), float64(yn) / float64(yd), true
 }
--- lines/lines_test.go
+++ lines/lines_test.go
@@ -7,43 +7,56 @@ import (
 	"github.com/jack-barr3tt/gostuff/types"
 )
 
+func TestNewFracMC(t *testing.T) {
+	test.AssertEqual(t, NewFracMC(1, 2, 3, 4), Line{1, 2, 3, 4})
+	test.AssertEqual(t, NewFracMC(2, 4, 6, 8), Line{1, 2, 3, 4})
+	test.AssertEqual(t, NewFracMC(1, -2, 1, 1), Line{-1, 2, 1, 1})
+}
+
 func TestNewYMXC(t *testing.T) {
-	test.AssertEqual(t, NewYMXC(1, 2), Line{1, 2})
+	test.AssertEqual(t, NewYMXC(1, 2), Line{1, 1, 2, 1})
 }
 
 func TestNewBetween(t *testing.T) {
-	test.AssertEqual(t, NewBetween(types.Point{1, 2}, types.Point{3, 4}), Line{1, 1})
+	test.AssertEqual(t, NewBetween(types.Point{1, 2}, types.Point{3, 4}), Line{1, 1, 1, 1})
 }
 
 func TestNewPointDir(t *testing.T) {
-	test.AssertEqual(t, NewPointDir(types.Point{1, 2}, types.Direction{2, 4}), Line{2, 0})
+	test.AssertEqual(t, NewPointDir(types.Point{1, 2}, types.Direction{2, 4}), Line{2, 1, 0, 1})
 }
 
 func TestNewAXBYC(t *testing.T) {
-	test.AssertEqual(t, NewAXBYC(94, 22, 8400), Line{-94.0 / 22.0, 8400.0 / 22.0})
+	test.AssertEqual(t, NewAXBYC(94, 22, 8400), Line{-47, 11, 4200, 11})
 }
 
 func TestSubX(t *testing.T) {
-	l := Line{1, 2}
+	l := Line{1, 1, 2, 1}
 	test.AssertEqual(t, l.SubX(3), 5)
 }
 
 func TestSubY(t *testing.T) {
-	l := Line{1, 2}
+	l := Line{1, 1, 2, 1}
 	test.AssertEqual(t, l.SubY(3), 1)
 }
 
 func TestIntersectAt(t *testing.T) {
-	l1 := Line{1, 2}
-	l2 := Line{0.5, 4}
+	l1 := Line{1, 1, 2, 1}
+	l2 := Line{1, 2, 4, 1}
 	x, y, ok := l1.IntersectsAt(l2)
 	test.AssertEqual(t, x, 4)
 	test.AssertEqual(t, y, 6)
 	test.AssertEqual(t, ok, true)
 
-	l3 := Line{1, 5}
+	l3 := Line{1, 1, 5, 1}
 	x, y, ok = l1.IntersectsAt(l3)
 	test.AssertEqual(t, x, 0)
 	test.AssertEqual(t, y, 0)
 	test.AssertEqual(t, ok, false)
+
+	l4 := NewAXBYC(94, 22, 8400)
+	l5 := NewAXBYC(34, 67, 5400)
+	x, y, ok = l4.IntersectsAt(l5)
+	test.AssertEqual(t, x, 80)
+	test.AssertEqual(t, y, 40)
+	test.AssertEqual(t, ok, true)
 }
